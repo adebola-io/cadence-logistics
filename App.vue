493998c1ce3855553f7789b78be39464a31e7f98
adebola-xyz <adebolaakomolafe@gmail.com>
2022-11-01 13:30:16 +0100

    Initial commit.

---
+++ App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+const r = 8;
+</script>
+
+<template>
+   <div>Hello World</div>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,6 @@
+import vue from "@vitejs/plugin-vue";
+
+/** @type import('vite').UserConfig */
+export default {
+   plugins: [vue()],
+};
