8a71d15eb2c018df580c8cba11bf7ed4b1623877
adebola-xyz <adebolaakomolafe@gmail.com>
2022-11-06 23:16:35 +0100

    Add page change transition

--- App.vue
+++ App.vue
@@ -5,6 +5,25 @@ import AppHeader from "./components/layout/AppHeader.vue";
 
 <template>
    <AppHeader />
-   <router-view></router-view>
+   <RouterView v-slot="{ Component }">
+      <Transition name="fade-leave" mode="out-in">
+         <component :is="Component"></component>
+      </Transition>
+   </RouterView>
    <AppFooter />
 </template>
+
+<style scoped>
+.fade-leave-enter-active,
+.fade-leave-leave-active {
+   transition-duration: 500ms;
+}
+.fade-leave-leave-to {
+   transform: translateX(10%);
+   opacity: 0;
+}
+.fade-leave-enter-from {
+   opacity: 0;
+   transform: translateX(-10%);
+}
+</style>
